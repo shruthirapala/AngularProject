fbcc4215263276e93e12d78f52cc92a151055fa8
Shruthi <shruthirapala99@gmail.com>
2022-02-28 14:27:26 -0500

    Added Validation to Product Edit Form

--- src/app/products/product-edit.component.ts
+++ src/app/products/product-edit.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChildren,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControlName,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from './product.service';
+import { GenericValidator } from '../shared/generic-validator';
+import { fromEvent, merge, Observable, Subscription } from 'rxjs';
+import { NumberValidators } from '../shared/number.validator';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'pm-product-edit',
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css'],
 })
-export class ProductEditComponent implements OnInit {
+export class ProductEditComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChildren(FormControlName, { read: ElementRef })
+  formInputElements!: ElementRef[];
+
   productForm!: FormGroup;
   pageTitle = 'Product Edit';
+  private sub!: Subscription;
+  displayMessage: { [key: string]: string } = {};
+  private validationMessages: { [key: string]: { [key: string]: string } };
+  private genericValidator: GenericValidator;
 
   constructor(
     private fb: FormBuilder,
@@ -18,6 +41,22 @@ export class ProductEditComponent implements OnInit {
     private productService: ProductService
   ) {
     // Defines all of the validation messages for the form.
+    this.validationMessages = {
+      productName: {
+        required: 'Product Name is Required',
+        minlength: 'Product name must be at least three characters.',
+        maxlength: 'Product name cannot exceed 50 characters.',
+      },
+      productCode: {
+        required: 'Product code is required.',
+      },
+      starRating: {
+        range: 'Rate the product between 1 (lowest) and 5 (highest).',
+      },
+    };
+
+    // Define the validation instance and pass the validation messages to the form
+    this.genericValidator = new GenericValidator(this.validationMessages);
   }
 
   ngOnInit(): void {
@@ -31,17 +70,40 @@ export class ProductEditComponent implements OnInit {
         ],
       ],
       productCode: ['', Validators.required],
-      starRating: ['', Validators.required],
-      Discription: '',
+      starRating: ['', NumberValidators.range(1, 5)],
+      description: '',
     });
 
-    // Reacd the product Id from route parameters
-    // this.sub = this.route.paramMap.subscribe((params) => {
-    //   const id = +params.get('id');
-    //   this.getProduct(id); //
-    // });
+    //Read the product Id from route parameters
+    this.sub = this.route.paramMap.subscribe((params) => {
+      const id = params.get('productId');
+      if (id) this.getProduct(+id);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    // Watch for the blur event from any input element on the form.
+    // This is required because the valueChanges does not provide notification on blur
+    const controlBlurs: Observable<any>[] = this.formInputElements.map(
+      (formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur')
+    );
+
+    // Merge the blur event observable with the valueChanges observable
+    // so we only need to subscribe once.
+    merge(this.productForm.valueChanges, ...controlBlurs)
+      .pipe(debounceTime(800))
+      .subscribe((value) => {
+        this.displayMessage = this.genericValidator.processMessages(
+          this.productForm
+        );
+      });
   }
 
   saveProduct() {}
   deleteProduct() {}
+  getProduct(id: number) {}
 }
